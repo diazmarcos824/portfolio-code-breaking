c064667ab39b8e4e9cf69c504b6667dbc4c7af28
Marcos Diaz <diazmarcosqa@gmail.com>
2024-04-01 20:30:05 -0500

    testimonials slider done

--- js/auth-glider.js
+++ js/auth-glider.js
@@ -1,6 +1,23 @@
+const wrapper = document.querySelector(".wrap");
 const slider = document.querySelector(".container__grid");
+const sliderChildrens = [...slider.children]
+const firstCardWidth = slider.querySelector(".grid__cards").offsetWidth;
 
-let isDragging = false, startX, startScrollLeft;
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+let cardPerView = Math.round(slider.offsetWidth / firstCardWidth);
+
+sliderChildrens.slice(-cardPerView).reverse().forEach(card => {
+    slider.insertAdjacentHTML("afterBegin", card.outerHTML)
+})
+
+sliderChildrens.slice(0, cardPerView).forEach(card => {
+    slider.insertAdjacentHTML("beforeEnd", card.outerHTML)
+})
+
+slider.classList.add("no-transition");
+slider.scrollLeft = slider.offsetWidth;
+slider.classList.remove("no-transition");
 
 const dragStart = (e) => {
     isDragging = true;
@@ -21,6 +38,34 @@ const dragStop = () => {
     slider.classList.remove("dragging");
 }
 
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(slider.scrollLeft === 0) {
+        slider.classList.add("no-transition");
+        slider.scrollLeft = slider.scrollWidth - (2 * slider.offsetWidth);
+        slider.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(slider.scrollLeft) === slider.scrollWidth - slider.offsetWidth) {
+        slider.classList.add("no-transition");
+        slider.scrollLeft = slider.offsetWidth;
+        slider.classList.remove("no-transition");
+    }
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => slider.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
 slider.addEventListener("mousedown", dragStart);
 slider.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
+slider.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
