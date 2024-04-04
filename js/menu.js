f22653f3624758e77e73d69e974ed6edb3b1e234
Marcos Diaz <diazmarcosqa@gmail.com>
2024-04-04 17:42:47 -0500

    better sizes

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,10 @@ const close = document.querySelector("#close");
 
 open.addEventListener("click", () => {
     nav.classList.add("nav--visible");
+    nav.classList.add("transition");
 })
 
 close.addEventListener("click", () => {
     nav.classList.remove("nav--visible");
+    nav.classList.remove("transition");
 })
